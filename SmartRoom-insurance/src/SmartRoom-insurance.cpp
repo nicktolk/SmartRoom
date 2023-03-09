d330a40883a925958beb52c3b6a00cd76a5aa5fc
Nick Tolk <nicktolk@gmail.com>
2023-03-09 16:15:57 -0700

    neopixel output

--- SmartRoom-insurance/src/SmartRoom-insurance.cpp
+++ SmartRoom-insurance/src/SmartRoom-insurance.cpp
@@ -11,29 +11,59 @@
  * Date:
  */
 
-void myHandler(const char *event, const char *data);
+#include "neopixel.h"
+
+void setTower(const char *event, const char *data);
 void setup();
 void loop();
-#line 8 "c:/Users/nick/Documents/IoT/SmartRoom/SmartRoom-insurance/src/SmartRoom-insurance.ino"
-const String accessToken = "ca7e850a1df7632615141b1c1e51c9de8e347da9";
-String tempAccess = "https://api.particle.io/v1/devices/e00fce6879a39f9da7886944/temp?access_token=ca7e850a1df7632615141b1c1e51c9de8e347da9";
+#line 10 "c:/Users/nick/Documents/IoT/SmartRoom/SmartRoom-insurance/src/SmartRoom-insurance.ino"
+const int minTemp = 73, maxTemp = 77;
 
-int i = 0;
+const int PIXEL_PIN = D2;
+const int PIXEL_COUNT = 46;
+#define PIXEL_TYPE WS2812B
 
-void myHandler(const char *event, const char *data)
+//const String accessToken = "ca7e850a1df7632615141b1c1e51c9de8e347da9";
+//String tempAccess = "https://api.particle.io/v1/devices/e00fce6879a39f9da7886944/temp?access_token=ca7e850a1df7632615141b1c1e51c9de8e347da9";
+
+int numLights;
+int r, g, b;
+uint32_t color;
+
+Adafruit_NeoPixel pixel ( PIXEL_COUNT , PIXEL_PIN , PIXEL_TYPE ); // declare object
+
+void pixelFill(int startPixel, int endPixel, uint32_t colorIn);
+
+float tempF;
+void setTower(const char *event, const char *data)
 {
-  i++;
-  Serial.printf("%d: event=%s data=%s", i, event, (data ? data : "NULL"));
+  tempF = atof(data);
+  numLights = map((int)tempF*100, minTemp*100, maxTemp*100, 0, PIXEL_COUNT);
+
+  b = map((int)tempF*100, minTemp*100, maxTemp*100, 0xff, 0);
+  b = (b < 0) ? 0 : (b > 0xff) ? 0xff : b;
+  g = 0;
+  r = 0xff - b;
+  color = ((r << 8) | g) << 8 | b;
+  
+  pixel.clear();
+  pixelFill(0, numLights, color);
+  pixel.show();
 }
 
 
 void setup() {
   Particle.connect();
-  Particle.subscribe("tempC", myHandler);
-//  Particle.function("getTemp", &getTemp);
+  Particle.subscribe("tempF", setTower);
+  pixel.setBrightness(15);
 }
 
 void loop() {
-//  Serial.printf("%f\n", tempC / 100.0);
-  delay(20000);
+  delay(1000);
+}
+
+void pixelFill(int startPixel, int endPixel, uint32_t colorIn){
+  for (int i = startPixel; i < endPixel; i++){
+    pixel.setPixelColor(i, colorIn);
+  }
 }
--- SmartRoom-insurance/src/SmartRoom-insurance.ino
+++ SmartRoom-insurance/src/SmartRoom-insurance.ino
@@ -5,25 +5,56 @@
  * Date:
  */
 
-const String accessToken = "ca7e850a1df7632615141b1c1e51c9de8e347da9";
-String tempAccess = "https://api.particle.io/v1/devices/e00fce6879a39f9da7886944/temp?access_token=ca7e850a1df7632615141b1c1e51c9de8e347da9";
+#include "neopixel.h"
 
-int i = 0;
+const int minTemp = 73, maxTemp = 77;
 
-void myHandler(const char *event, const char *data)
-{
-  i++;
-  Serial.printf("%d: event=%s data=%s", i, event, (data ? data : "NULL"));
-}
+const int PIXEL_PIN = D2;
+const int PIXEL_COUNT = 46;
+#define PIXEL_TYPE WS2812B
 
+//const String accessToken = "ca7e850a1df7632615141b1c1e51c9de8e347da9";
+//String tempAccess = "https://api.particle.io/v1/devices/e00fce6879a39f9da7886944/temp?access_token=ca7e850a1df7632615141b1c1e51c9de8e347da9";
+
+int numLights;
+int r, g, b;
+uint32_t color;
+
+Adafruit_NeoPixel pixel ( PIXEL_COUNT , PIXEL_PIN , PIXEL_TYPE ); // declare object
+
+void pixelFill(int startPixel, int endPixel, uint32_t colorIn);
+
+float tempF;
+void setTower(const char *event, const char *data);
 
 void setup() {
   Particle.connect();
-  Particle.subscribe("tempC", myHandler);
-//  Particle.function("getTemp", &getTemp);
+  Particle.subscribe("tempF", setTower);
+  pixel.setBrightness(15);
 }
 
 void loop() {
-//  Serial.printf("%f\n", tempC / 100.0);
-  delay(20000);
+  delay(1000);
+}
+
+void pixelFill(int startPixel, int endPixel, uint32_t colorIn){
+  for (int i = startPixel; i < endPixel; i++){
+    pixel.setPixelColor(i, colorIn);
+  }
+}
+
+void setTower(const char *event, const char *data)
+{
+  tempF = atof(data);
+  numLights = map((int)tempF*100, minTemp*100, maxTemp*100, 0, PIXEL_COUNT);
+
+  b = map((int)tempF*100, minTemp*100, maxTemp*100, 0xff, 0);
+  b = (b < 0) ? 0 : (b > 0xff) ? 0xff : b;
+  g = b;
+  r = 0xff - b;
+  color = ((r << 8) | g ) << 8 | b;
+  
+  pixel.clear();
+  pixelFill(0, numLights, color);
+  pixel.show();
 }
